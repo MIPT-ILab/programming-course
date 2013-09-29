571bb80bbcfa4b373fba71a98439b2dc57536399
Gar <logirz728@gmail.com>
2013-09-29 20:02:20 +0400

    Reformatted, threw in more defines

--- students/garipov_ilias/Squares.cpp
+++ students/garipov_ilias/Squares.cpp
@@ -1,80 +1,141 @@
+/**
+*	@file    Squares.cpp
+*	@date    2013-09-29 19:00
+*	@author  Il'yas Garipov <logirz728@gmail.com>
+*
+*	Quadratic equations solving apparatus.
+*
+*	@par The input consists of 3 coefficients of
+*	square equation. Output usually consists of two rows of
+*	numbers: 'Number of roots' and 'Roots', where
+*	'Roots' may hold up to two numbers, or not exist at all
+*	if there aren't any.
+*/
+
+
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
+#include <assert.h>
+
+#define IS_ZERO( x ) (fabs(x) < DBL_EPSILON)
+#define IS_BELOW_ZERO( x ) (x < -DBL_EPSILON)
+
+#ifndef NDEBUG
+	#define DOUT( toprint ) printf( "\n# " toprint )
+	#define DPUT( toput , x ) printf( "\n# " toput , x )
+	#define DLN
+	#define DPUTCLOSURE DOUT("=============================================================================");\
+                        DPUT("FILE: %s", __FILE__ );\
+                        DPUT("TIME OF COMPILATION: %s", __TIME__ )
+#else
+	#define DOUT
+	#define DPUT
+	#define DLN if(0)
+	#define DPUTCLOSURE
+#endif
+
+
+
 
 int SolvSqr(float a, float b, float c,
             float* x1, float* x2);
 
-const int ALLRTS = -1; // A state where any real x can be inserted as a root in the equation
+const int ALLRTS = -1; //!< SolveSqr returns this if any real X is a root
 
 int main()
 {
-    printf(
-        "#YOUR_REQUEST: Quadratic equations solver\n"
-        "================================================================================ \n"
-        "Enter 3 numbers that correspond to a, b and c in the equation \n"
-        "\t a*x^2 + b*x + c = 0 \n"
-        ">"
-    );
+	DOUT("YOUR_REQUEST: Quadratic equations solver");
+	DOUT("=============================================================================");
+	DOUT("Enter 3 numbers that correspond to a, b and c in the equation");
+	DOUT("\t a*x^2 + b*x + c = 0");
+	DOUT(">");
 
-    float a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-    scanf("%f %f %f", &a, &b, &c);
+	float a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+	scanf("%f %f %f", &a, &b, &c);
 
-    int Rts = SolvSqr(a, b, c, &x1, &x2);
+	int Rts = SolvSqr(a, b, c, &x1, &x2);
 
-    if(fabs(x1) < DBL_EPSILON) x1 = 0;
+	if( IS_ZERO(x1) ) x1 = 0;
+	if( IS_ZERO(x2) ) x2 = 0;
 
-    if(fabs(x2) < DBL_EPSILON) x2 = 0;
+	DLN printf("\n");		//!< For a neatly formatted output
+	printf("%d\n", Rts);
 
-    switch(Rts) {
-        case(0) :
-            printf("There are no roots in real numbers for the given equation\n");
-            break;
+	switch(Rts) {
+		case(0) :
+			DOUT("There are no roots in real numbers for the given equation\n\n");
+			break;
 
-        case(1) :
-            printf("There is only one root %lg for the given equation\n", x1);
-            break;
+		case(1) :
+			DOUT("There is only one root for the given equation:\n\n");
+			printf("%lg", x1);
+			break;
 
-        case(2) :
-            printf("The given equation possesses two roots in real numbers: %lg and %lg \n", x1, x2);
-            break;
+		case(2) :
+			DOUT("The given equation possesses two roots in real numbers:\n\n");
+			printf("%lg %lg", x1, x2);
+			break;
 
-        case(ALLRTS) :
-            printf("Any real number can be a root of the given equation\n");
-    }
+		case(ALLRTS) :
+			DOUT("Any real number can be a root of the given equation\n\n");
+			break;
+	}
 
-    printf(
-        "================================================================================\n"
-        "Request processed and result delivered.\n"
-        "\tRestart application to process another quadratic equation."
-    );
-
-    return 0;
+	DLN printf("\n");		//!< Same as on the line 62.
+	DOUT("=============================================================================");
+	DOUT("Request processed and result delivered.");
+	DOUT("\tRestart application to process another quadratic equation.");
+	DPUTCLOSURE;
+	return 0;
 }
 
+/**
+*		SolveSqr - solve a quadratic equation defined by its coefficients
+*
+*	@note 1)In documentation below, (1) is a*x^2 + b*x + c = 0;
+*		and if one or both roots do not exist, or if there is
+*		an infinite number of them, we do nothing with x1 and x2.
+*
+*		2)Also, we take calc precision as DBL_EPSILON
+*
+*	@param		a	The 'a' in (1)
+*	@param		b	The 'b' in (1)
+*	@param		c	The 'c' in (1)
+*	@param[out]	x1	First root
+*	@param[out]	x2	Second root
+*
+*	@return If roots exist, their quantity.
+*	If there are none, 0.
+*	If there are all the roots in the world, -1.
+*
+*/
 int SolvSqr(float a, float b, float c,
             float* x1, float* x2)
 {
+	DLN assert(( "We've processed pointer to x1 alright", (x1 != NULL) ));
+	DLN assert(( "We've processed pointer to x2 alright, too", (x2 != NULL) ));
 
-    if(fabs(a) > DBL_EPSILON) {
-        float D = b * b - (4 * a * c);
+	if( !(IS_ZERO(a)) ) {
+		float D = b * b - (4 * a * c);
 
-        if(D < 0) return 0;
-        else if(fabs(D) < DBL_EPSILON) {
-            *x1 = (-b) / (2 * a);
-            return 1;
-        } else {
-            D = sqrt(D);
-            *x1 = (-b - D) / (2 * a);
-            *x2 = (-b + D) / (2 * a);
-            return 2;
-        }
-    } else if(fabs(b) > DBL_EPSILON) {
-        *x1 = (-c) / b;
-        return 1;
-    } else if(fabs(c) > DBL_EPSILON) {
-        return 0;
-    } else
-        return ALLRTS;
+		if(IS_BELOW_ZERO(D)) return 0;
+
+		else if(IS_ZERO(D)) {
+			*x1 = (-b) / (2 * a);
+			return 1;
+		} else {
+			D = sqrt(D);
+			*x1 = (-b - D) / (2 * a);
+			*x2 = (-b + D) / (2 * a);
+			return 2;
+		}
+	} else if( !(IS_ZERO(b)) ) {
+		*x1 = (-c) / b;
+		return 1;
+	} else if( !(IS_ZERO(c)) ) {
+		return 0;
+	} else
+		return ALLRTS;
 
 }
