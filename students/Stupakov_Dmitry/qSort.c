11d58ecf8b1d75c76e1fc2008406bf91966f1d4e
Dmitry_Stupakov <dmitry.stupakov@frtk.ru>
2013-10-01 00:21:10 +0400

    Array sort 1.0

--- students/Stupakov_Dmitry/qSort.c
+++ students/Stupakov_Dmitry/qSort.c
@@ -8,17 +8,16 @@ int Minimum(int *p, int *q)
 int main()
 {
 	int i = 0, n = 0, a[30000] = {0*30000};
-	printf("Sorting an array of integers\n"
-		   "Author Dmitry Stupakov\n"
-		   "Version 1.0\n"
-		   "30.09.2013\n"
-		   "Enter the number of array elements: ");
+	printf("#Author Dmitry Stupakov\n"
+		   "#Version 1.0\n"
+		   "#30.09.2013\n"
+		   "#Enter the number of array elements:\n");
 	scanf_s("%i", &n);
-	printf("Elements: ");
+	printf("#Elements:\n");
     for(i=0; i<n; i++)
 	    scanf_s("%i", &a[i]);
     qsort(a, n, sizeof(int), Minimum);
-	printf("Sorted array: ");
+	printf("#Sorted array:\n");
     for(i=0;i<n;i++)
 	    printf("%i ", a[i]);
 	printf("\n");
