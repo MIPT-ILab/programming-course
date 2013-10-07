486e3ffd998c9b9ec10cf508415202424f367d43
Hanna Saenko <HannaSanoke@gmail.com>
2013-10-07 20:59:13 +0300

    Добавлены упущенные ранее случаи решения уравнения, комментарии. Отформатирован код.

--- students/Saenko_Anna/SSquare.c
+++ students/Saenko_Anna/SSquare.c
@@ -15,19 +15,28 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+
+
     int SolveSquare(double a, double b, double c, double* x1, double* x2);
+
     int main()
-    { double x1, x2;
+    {
+      double x1, x2;
       double a, b, c;
+
       printf("# Enter coefficients\n");
       scanf("%lf %lf %lf",&a,&b,&c);
+
       int nRoots=SolveSquare(a,b,c,&x1,&x2);
+
       printf("# Equation has %i roots:\n",nRoots);
+
       if(nRoots==2)
         {printf("%e %e\n",x1,x2);}
       else
-        { if(nRoots==1) {printf("%e\n",x1);}
-          else {printf("# Equation has infinite roots \n");  }
+        {
+          if(nRoots==1) {printf("%e\n",x1);}
+          if(nRoots==-1) {printf("# Equation has infinite roots \n");}
         }
     }
 
@@ -42,22 +51,40 @@
 //!
 //! @return         Number of roots or zero if none, -1 if infinite number of roots
 //}=================================================================================
+
     int SolveSquare(double a, double b, double c, double  *x1, double *x2)
-    { assert(x1!=NULL); assert(x2!=NULL); assert(x1!=x2);   //Precondition tests
+    {
+      //Precondition tests
+      assert(x1!=NULL);
+      assert(x2!=NULL);
+      assert(x1!=x2);
+
       double D=b*b-4*a*c;
+
       if((a==0)&&(b!=0)&&(c!=0))
-      {*x1=-c/b;
-        return 1;}
+        {
+          *x1=-c/b;
+          return 1;
+        }
+
       if(a==b==c==0)
-      {return -1;}
+        {return -1;}
+
       if(D>0)
-      { *x1=(-b+sqrt(D))/(2*a);
-        *x2=(-b-sqrt(D))/(2*a);
-        return 2;
-      }
+        {
+          *x1=(-b+sqrt(D))/(2*a);
+          *x2=(-b-sqrt(D))/(2*a);
+          return 2;
+        }
       else
-      {  if(D==0) { *x1=-b/(2*a); return 1;}
-         else return 0; }
+        {
+         if(D==0)
+         {
+           *x1=-b/(2*a);
+           return 1;
+         }
+         else return 0;
+        }
     }
 
 
