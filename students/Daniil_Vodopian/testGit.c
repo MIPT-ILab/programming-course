0227593796980bbcebc4ea8c62bb583b6ca14f6e
voddan <dgvodopyan@gmail.com>
2013-10-19 21:08:15 +0400

    I'm trying to commit something. Do not worry if you see this message.

---
+++ students/Daniil_Vodopian/testGit.c
@@ -0,0 +1,7 @@
+//it's a simple program just to test GIT system
+#include <stdio.h>
+
+int main(){
+	printf("Hello, ILab! Pleased to be launched here!\n I've got it!\n");
+	printf("one more test\n");
+}
