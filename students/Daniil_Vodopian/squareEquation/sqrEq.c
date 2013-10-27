4451d0ee7a4253a1c9fb792991b5bb559e973e2b
voddan <dgvodopyan@gmail.com>
2013-10-27 15:45:01 +0400

    sqrEq 1.02

--- students/Daniil_Vodopian/squareEquation/sqrEq.c
+++ students/Daniil_Vodopian/squareEquation/sqrEq.c
@@ -1,8 +1,8 @@
 /** ********************************************************************
  @file		sqrEq.c
- @date		2013-10-23 14:45
+ @date		2013-10-27 15:08
  @author	Daniil Vodopian <dgvodopyan@gmail.com>
- @version	1.01
+ @version	1.02
 
  Solving a square equation with minding all cases.
 
@@ -11,9 +11,9 @@
 			ouput: number of its roots (-1 in infinity) 
 					* and the roots themselves (if exist)
 			
- @return 	0: no errors occurred
-			1: an unknown exception happened
-			2: incorrect input
+ @return 	0: NoErr		no errors occurred
+			1: UnknownErr	an unknown exception happened
+			2: InputErr		incorrect input
 			
  @note		The precision of output is the second digit after the decimal point
 			
@@ -21,6 +21,7 @@
 			It will close by itself after an incorrect input.
 			If you want to stop using the program, enter empty line when asked.
 ***********************************************************************/
+typedef enum {NoErr = 0, UnknownErr = 1, InputErr = 2} MainReturn_t;
 //----------------------------------------------------------------------
 #include <stdio.h>
 #include <math.h>
@@ -55,12 +56,17 @@
  @param[out] x1	1st root of equation, if exist (if not, value will be unchanged)
  @param[out] x2	2nd root of equation, if exist (if not, value will be unchanged)
  
- @return		Number of roots or zero if none, -1 if infinite number of roots
+ @return	-1: InfRoots	Infinitive number of roots
+			 0: NoRoots		No roots exist
+			 1: OneRoot		One root
+			 2: TwoRoots	Two roots
+			13: Error		Any error
  
  @note			Calculation precision is considered to be FLT_EPSILON.
 ***********************************************************************/
-int solveSquare (float a, float b, float c, float* x1, float* x2);
-
+typedef enum {InfRoots = -1, NoRoots = 0, OneRoot = 1, TwoRoots = 2, Error = 13} SolveSquareReturn_t;
+SolveSquareReturn_t solveSquare (float a, float b, float c, float* x1, float* x2);
+//----------------------------------------------------------------------
 /** ********************************************************************
  printEquation - print a square equation specified by its coefficients
 					* in the form a*(x^2) + b*x + c = 0
@@ -75,14 +81,14 @@ void printEquation (float a, float b, float c);
 //----------------------------------------------------------------------
 //MAIN FUNCTION
 int main(){
-	printf(	"#I will solve your square equation\n");
-	printf(	"#Enter three float value, separeted by a space " 
+	printf(	"#sqrEq.c 1.02 Daniil Vodopian\n" "#Solve a square equation\n");
+	printf(	"#Enter three float values, separeted by a space " 
 			"- A, B and C coeficients " 
 			"in a square euation A*(X^2) + B*X + C = 0\n");
 	printInvit;
 			
-	float A = 0, B = 0, C = 0; 	//coeficients
-	switch (scanf("%f %f %f", &A, &B, &C)){	//read input and catch errors
+	float A = 0, B = 0, C = 0;
+	switch (scanf("%f %f %f", &A, &B, &C)){
 		case EOF: 
 			printErr("no float values entered"); return 2; break;
 		case 0: case 1: case 2:		//0, 1 or 2 values in the input
@@ -97,45 +103,46 @@ int main(){
 	printEquation(A, B, C);
 	printf("\n");
 	
-	float X1 = 0, X2 = 0;							//roots
-	int rootNum = solveSquare(A, B, C, &X1, &X2); 	//number of roots
+	float X1 = 0, X2 = 0;
+	SolveSquareReturn_t rootNum = solveSquare(A, B, C, &X1, &X2);
 	printf("#Number of different roots this equation has:\n" "%d\n", rootNum);
 	switch (rootNum){
-		case -1: printf("#Every real number is a root\n");								break;
-		case  0: printf("#There is NO roots at all\n");									break;
-		case  1: printf("#There is only ONE root:\n" " %.2f", X1);						break;
-		case  2: printf("#There are TWO defferent roots:\n" " %.2f;  %.2f;", X1, X2); 	break;
-		default: printErr("an unknown eror in solving the equation\n"); return 1; 		break;
+		case InfRoots:	printf("#Every real number is a root\n");									break;
+		case NoRoots:	printf("#There is NO roots at all\n");										break;
+		case OneRoot:	printf("#There is only ONE root:\n" " %.2f\n", X1);							break;
+		case TwoRoots:	printf("#There are TWO defferent roots:\n" " %.2f;  %.2f;\n", X1, X2);		break;
+		case Error: default: 
+						printErr("an unknown eror in solving the equation\n"); return UnknownErr;	break;
 	}
 	
-	printf(	"#Thank you for using me to solve your square equation\n");
-	return 0;	//everything was OK
+	//printf(	"#Thank you for using me to solve your square equation\n");
+	return NoErr;
 }
 
 //----------------------------------------------------------------------
-int solveSquare (float a, float b, float c, float* x1, float* x2){
+SolveSquareReturn_t solveSquare (float a, float b, float c, float* x1, float* x2){
 	//a*(x^2) + b*x + c = 0		//general form of the equation
 	float d = b*b - 4*a*c;		//discriminant of this equation (if it is a SQUARE one)
 	
-	if 		(!a && !b && !c)	return -1;	//0 = 0
-	else if (!a && !b &&  c)	return  0;	//0 = c
-	else if (!a){ 							//it is a LINEAR equation
+	if 		(!a && !b && !c)	return InfRoots;	//0 = 0
+	else if (!a && !b &&  c)	return NoRoots;		//0 = c
+	else if (!a){ 
 		*x1 = -c / b;						//b*x = -c
-		return  1;
+		return  OneRoot;
 	}
-	else if (d < 0)				return  0;	//NO  roots
-	else if (d == 0){						//ONE root
-		*x1 = -b / (2*a);			
-		return  1;
+	else if (d < 0)				return  NoRoots;
+	else if (d == 0){
+		*x1 = -b / (2*a);
+								return  OneRoot;
 	}
-	else{									//TWO roots
+	else{
 		float sqrtD = sqrt(d);
 		*x1 = (-b - sqrtD) / (2*a);
 		*x2 = (-b + sqrtD) / (2*a);
-		return 2;
+								return TwoRoots;
 	}
 	
-	return -1;
+								return Error;
 }
 //----------------------------------------------------------------------
 #define signStr(x) 	(	\
@@ -144,7 +151,7 @@ int solveSquare (float a, float b, float c, float* x1, float* x2){
 	(x > 0)  ? "+ " : "") //return string with the sign of x
 
 void printEquation (float a, float b, float c){
-	if(a || b || c){							//a coef. is NOT zero
+	if(a || b || c){				//a coef. is NOT zero
 		if(a){										//print a-coef
 			printf("%s", (a < 0)? "- ": "");					//print sign if required
 			if(fabs(a) == 1) 	printf("(X^2)");				//a-coef is NOT required
@@ -161,6 +168,6 @@ void printEquation (float a, float b, float c){
 			printf(" %s", ((!a && !b && c < 0) || (a || b))? signStr(c): "");	//print sign if required
 			printf("%.2f", fabs(c)); 											//print c-coef without sign
 		}
-	}else 		printf("0");					//all coef. are zero
+	}else 		printf("0");		//all coef. are zero
 	printf(" = 0");
 }//*/
