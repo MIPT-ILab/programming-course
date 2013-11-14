49105b1ed8e9296a61fb614f0ca63b8e5397726a
Semenov <racer211295@gmail.com>
2013-11-15 03:35:45 +0400

    Working version

--- students/Semenov_Nikolay/cpu.c
+++ students/Semenov_Nikolay/cpu.c
@@ -1,90 +1,96 @@
-#define ST_SIZE 256
-#define NUM_CMD 256
-#define NUM_LBL 9
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
 
+#define ST_SIZE 256
+#define NUM_CMD 256
+#define NUM_LBL 9
+
 //------------------------------------------STACK---------------------------------
 
-        static int *top      ;
-        static int *ch_start ;
+struct stack
+{
+	int *top      ;
+	int *ch_start ;
+};
 
-  
-
-/*int ok_stack()//Stack Check
+int ok_stack(struct stack *st)//Checking pointers
 {        
-        return((ch_start <= top) && (top < ch_start + ST_SIZE)) ;
-}
-*/
+        assert(st->ch_start <= st->top)          ;
+        assert(st->top < st->ch_start + ST_SIZE) ;
 
-int stack_construct()//Construct Stack
+        return 0                                 ;
+}
+
+
+int stack_construct(struct stack *st)
 {
-        ch_start = (int*)calloc(ST_SIZE, sizeof(int)) ;
-        top = ch_start   ;
+        st->ch_start = (int*)calloc(ST_SIZE, sizeof(int)) ;
+        st->top = st->ch_start + 1 ;
           
-        return 0         ;
+        return 0               ;
 }
 
-int stack_destruct()//Destruct Stack
+int stack_destruct(struct stack *st)
 {
-        free(ch_start)    ;
-        *ch_start = 0xBAD ;
-        *top = *ch_start  ;          
+        free(st->ch_start) ;        
 
-        return 0          ;
+        return 0           ;
 }
 
 
-int push_stack(int elem)//Push smth into stack
+int push_stack(struct stack *st, int elem)
 {
-        assert(top < ch_start + ST_SIZE ) ;  
-        ++top                             ;
-        *top = elem                       ; 
+        ok_stack(st)       ;  
+        ++st->top          ;
+        *(st->top) = elem  ;
+        ok_stack(st)       ; 
+        printf("Done!\n");
           
-        return 0                          ;
+        return 0           ;
 }
 
-int pop_stack()//Pop smth outside stack
+int pop_stack(struct stack *st)
 {
-        assert(ch_start <= top);        
-        int val = *top         ;          
-        --top                  ;        
+        ok_stack(st)           ;        
+        int val = *(st->top)   ;          
+        --st->top              ;
+        ok_stack(st)           ;  
+        printf("Yep!\n");      
             
         return val             ;
 }
 //-----------------------------------------Handlers-----------------------
-int add()//Add last+prev.
+int add(struct stack *st)//Add last+prev.
 {
-        int val_1 = pop_stack() ;
-        int val_2 = pop_stack() ;
-        push_stack(val_1+val_2) ;
+        int val_1 = pop_stack(st)   ;
+        int val_2 = pop_stack(st)   ;
+        push_stack(st, val_1+val_2) ;
 
-        return 0                ;
+        return 0                    ;
 }
 
-int mul()//Multiply last and prev.
+int mul(struct stack *st)//Multiply last and prev.
 {
-        int val_1 = pop_stack() ;
-        int val_2 = pop_stack() ;
-        push_stack(val_1*val_2) ;
+        int val_1 = pop_stack(st)   ;
+        int val_2 = pop_stack(st)   ;
+        push_stack(st, val_1*val_2) ;
 
-        return 0                ;
+        return 0                    ;
 }
 
-int out()//Print the last element
+int out(struct stack *st)//Print the last element
 {
-	int last = pop_stack()	;
-	printf("%d\n", last)	;	
+	int last = pop_stack(st)	;
+	printf("%d\n", last)	    ;	
 	
-	return 0		        ;
+	return 0		            ;
 }
 
-int comp()//Compare last+prev
+int comp(struct stack *st)//Compare last+prev
 {
-	int val_1 = pop_stack() ;
-	int val_2 = pop_stack() ;
+	int val_1 = pop_stack(st) ;
+	int val_2 = pop_stack(st) ;
 	if(val_2 > val_1) 
 	{
 		return 1 ;
@@ -94,83 +100,107 @@ int comp()//Compare last+prev
 		return 0 ;
 	}
 }
+
+int data_lbls_constr(int *data, int num)//Filling the data of labels with -1
+{
+	for(int j = 0 ; j < num ; j++)
+	{
+		data[j] = -1 ;
+	}
+
+	return 0 ;
+}
 //------------------------------------------CPU-----------------------------
 
 int main()
 {
-  int ax,bx,cx,dx   ;
-  int cmd           ;
-  int fl = 0        ;
-  int data[NUM_CMD] ;
-  int lbl[NUM_LBL]  ;
-
-  for(int j = 0 ; j < NUM_LBL ; j++)
-  {
-  	lbl[j] = -1 ;
-  }
+  int ax,bx,cx,dx      ;
+  int cmd              ;
+  int fl = 0           ;
+  int data[NUM_CMD]    ;
+  int lbl[NUM_LBL]     ;
   
-  //Open ASM_out for reading
+  const JMP_CMD = -1   ;
+  const END = 9        ;
+    
+//****Open ASM_OUT for reading
+  FILE *stream, *fopen() ;
 
-  int i = 0 ;
-  while(EOF)
+  stream = fopen("ASM_OUT", "r") ;
+
+  if (stream == NULL)//Checking opening file
+  {  
+  	printf("Can't open ASM_OUT\n") ;
+	exit(1) ;
+  }
+//****
+
+  struct stack MyStack ;
+
+  data_lbls_constr(&lbl[0], NUM_LBL) ;  
+
+  int counter = 0 ;
+  int stop = 0    ;
+  while(stop != END)
   {
-  	scanf("%d", &data[i]);
-  	i++;
-  	if(data[i-1] == 37) lbl[data[i]] = i + 1 ;  
+  	fscanf(stream ,"%d", &data[counter]);
+  	if(data[counter] == 9) break;
+  	counter++;
+  	if(data[counter-1] == JMP_CMD) lbl[data[counter]] = counter + 1 ; 
   }	 
   
-  stack_construct();
+  stack_construct(&MyStack);
   
-  for(int j = 0 ; j < i ; j++ )
+  for(int step = 0 ; step < counter ; step++ )
   {   
-  	switch (data[j])
+  	switch (data[step])
 	{
-		case 0: j++ ;
-	         	switch(data[j])//Push register
+		case 0: step++ ;
+	         	switch(data[step])//Push register
 		   	    {
-		   	 		case 0 : push_stack(ax) ; break        ;
-		   			case 1 : push_stack(bx) ; break        ;
-		   			case 2 : push_stack(cx) ; break        ;
-		   			case 3 : push_stack(dx) ; break        ;
-		   			default: assert('Wrong argument' == 0) ; 
+		   	 		case 0 : push_stack(&MyStack, ax) ; break        ;
+		   			case 1 : push_stack(&MyStack, bx) ; break        ;
+		   			case 2 : push_stack(&MyStack, cx) ; break        ;
+		   			case 3 : push_stack(&MyStack, dx) ; break        ;
+		   			default: assert('Wrong register' == 0) ; 
 							break                          ;  
 		   	 	}
 			 	break ;
-		case 1: j++ ;//Push value 
-			 	push_stack(data[j]) ;
+		case 1: step++ ;//Push value 
+			 	push_stack(&MyStack, data[step]) ;
 			 	break ;
-		case 2: j++ ;//Move into register
-                switch(data[j])
+		case 2: step++ ;//Move into register
+                switch(data[step])
 		  	 	{
-		  			case 0: ax = pop_stack() ; break       ; 
-		  			case 1: bx = pop_stack() ; break       ;
-		  			case 2: cx = pop_stack() ; break       ;
-		  			case 3: dx = pop_stack() ; break       ;
-		  			default: assert('Wrong argument' == 0) ;
+		  			case 0: ax = pop_stack(&MyStack) ; break       ; 
+		  			case 1: bx = pop_stack(&MyStack) ; break       ;
+		  			case 2: cx = pop_stack(&MyStack) ; break       ;
+		  			case 3: dx = pop_stack(&MyStack) ; break       ;
+		  			default: assert('Wrong register' == 0) ;
 		           			break                          ;  
 		  	 	}
 			 	break ;
-		case 3: add() ;
+		case 3: add(&MyStack) ;
 			 	break ;
-		case 4: mul() ;
+		case 4: mul(&MyStack) ;
 			 	break ;
-	  	case 5: j++ ;//JMP
+	  	case 5: step++ ;//JMP
 	  			assert('Wrong label' == 0);
-	  			j = lbl[data[j]] ;
+	  			step = lbl[data[step]] ;
 	  			break ;
-		case 6: if(comp())//JBE
+		case 6: if(comp(&MyStack))//JBE
 				{
-					j++ ;
+					step++ ;
 					assert('Wrong label' == 0);
-					j = lbl[data[j]] ;
+					step = lbl[data[step]] ;
 				}
-				else j++ ;
+				else step++ ;
 				break;
-		case 7: j++ ;
-				lbl[data[j]] = j + 1 ;
-	  	case 8: out() ;//OUT
+		case 7: step++ ;
+				lbl[data[step]] = step + 1 ;
+	  	case 8: out(&MyStack) ;//OUT
 			 	break ;		
-		case 9: j = i ;//END
+		case 9: step = counter ;//END
 			 	break ; 
 		default: assert('Wrong argument' == 0) ; 
 			 	break  ;	
--- students/Semenov_Nikolay/transl.c
+++ students/Semenov_Nikolay/transl.c
@@ -7,66 +7,94 @@
 
 int main()
 {
-	//Open ASM_in for reading
-	// Open ASM_out for writing
+//****Open ASM_OUT for reading
+  FILE *open, *fopen() ;
+
+  open = fopen("ASM_IN", "r") ;
+
+  if (open == NULL)//Checking opening file
+  {  
+  	printf("Can't open ASM_IN\n") ;
+	exit(1) ;
+  }
+//****
+	
+//****Open ASM_OUT for reading
+  FILE *stream, *fopen() ;
+
+  stream = fopen("ASM_OUT", "w") ;
+
+  if (stream == NULL)//Checking opening file
+  {  
+  	printf("Can't open ASM_OUT\n") ;
+	exit(1) ;
+  }
+//****
 /*-------------------------------------------------
 Add assert, to check command name!
 -------------------------------------------------*/
 	char cmd[CMD_SIZE] ;
 	char reg[REG_SIZE] ;
 	int cmd_n, check, val, reg_n ;
+	int f = 1;
 	
-	while(EOF)
+	while(f)
 	{
-		scanf("%s", &cmd);
-		if(cmd[1] == 'U') cmd_n = 0  ; //Push 
-		if(cmd[1] == 'O') cmd_n = 1  ; //Pop
-		if(cmd[1] == 'D') cmd_n = 2  ; //Add
-		if(cmd[1] == 'U') cmd_n = 3  ; //Mul
-		if(cmd[1] == 'M') cmd_n = 4  ; //JMP
-		if(cmd[1] == 'B') cmd_n = 5  ; //JBE
-		if(cmd[0] == ':') cmd_n = 6  ; // :
-		if(cmd[1] == 'N') cmd_n = 7  ; //End
+		fscanf(open,"%s", &cmd);
+
+		if(cmd[1] == 'U') cmd_n = 3 ; //Mulv
+		if(cmd[0] == 'P') cmd_n = 0 ; //Push
+		if(cmd[1] == 'O') cmd_n = 1 ; //Pop
+		if(cmd[1] == 'D') cmd_n = 2 ; //Add
+		if(cmd[1] == 'M') cmd_n = 4 ; //JMP
+		if(cmd[1] == 'B') cmd_n = 5 ; //JBE
+		if(cmd[0] == ':') cmd_n = 6 ; // :
+		if(cmd[2] == 'T') cmd_n = 7 ; //OUT
+		if(cmd[1] == 'N') cmd_n = 8 ; //End
+		if(cmd[0] == 'C') cmd_n = 9 ; //Close
 
 		switch(cmd_n)
 		{
-			case 0: check = scanf("%d", val) ;
+			case 0: check = fscanf(open,"%d", &val) ;
 				switch(check)
 				{
-					case 0: scanf("%s", &reg) ;
+					case 0: fscanf(open,"%s", &reg) ;
 						if(reg[0] == 'A') reg_n = 0 ; //AX
 						if(reg[0] == 'B') reg_n = 1 ; //BX
 						if(reg[0] == 'C') reg_n = 2 ; //CX	
-						if(reg[0] == 'D') reg_n = 4 ; //DX
-						printf(" 0 %d", reg_n); //PUSH REG
+						if(reg[0] == 'D') reg_n = 3 ; //DX
+						fprintf(stream," 0 %d", reg_n); //PUSH REG
 						break ;
-					case 1: printf(" 1 %d", val) ; //PUSH VALUE
+					case 1: fprintf(stream," 1 %d", val) ; //PUSH VALUE
 						break ;
 					default: assert('Push is dropped' == 0) ;
 				} 
 				break ;
-			case 1: scanf("%s", &reg) ;
+			case 1: fscanf(open,"%s", &reg) ;
 				if(reg[0] == 'A') reg_n = 0 ; //AX
 				if(reg[0] == 'B') reg_n = 1 ; //BX
 				if(reg[0] == 'C') reg_n = 2 ; //CX	
-				if(reg[0] == 'D') reg_n = 4 ; //DX
-				printf(" 2 %d", reg_n); //POP
+				if(reg[0] == 'D') reg_n = 3 ; //DX
+				fprintf(stream," 2 %d", reg_n); //POP
 				break ;
-			case 2: printf(" 3 ") ; break ;//ADD
-			case 3: printf(" 4 ") ; break ;//MUL
-			case 4: scanf("%d", &val) ;
-				printf(" 5 %d", val) ; //JMP
+			case 2: fprintf(stream," 3 ") ; break ;//ADD
+			case 3: fprintf(stream," 4 ") ; break ;//MUL
+			case 4: fscanf(open,"%d", &val) ;
+				fprintf(stream," 5 %d", val) ; //JMP
 				break ;
-			case 5: scanf("%d", &val) ;
-				printf(" 6 %d", val) ; //JBE 
+			case 5: fscanf(open,"%d", &val) ;
+				fprintf(stream," 6 %d", val) ; //JBE 
 				break ;
-			case 6: printf(" 7 %c", cmd[2]) ; //:LABEL
+			case 6: fprintf(stream," -1 %c", cmd[1]) ; //:LABEL
 				break ;
-			case 7: printf(" 8 ") ; //OUT
+			case 7: fprintf(stream," 8 ") ; //OUT
 				break ;
-			case 8: printf(" 9 ") ; //END
+			case 8: fprintf(stream," 9 ") ; //END
 				break ;
-			default: assert('Wrong argument' == 0) ;
+			case 9: f = 0 ;
+				break ; 
+			default: assert('Wrong argument' == 0) ; 
+				break ;
 		}   
 	}
 	return 0;
