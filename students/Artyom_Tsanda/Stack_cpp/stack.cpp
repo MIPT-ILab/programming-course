3c8102ad1ec586a84bb425ca1aaad62992c30ded
Sergey Bykov <captaintrunky@gmail.com>
2014-03-07 21:00:57 +0400

    Review for A. Tsanda, stack cpp

--- students/Artyom_Tsanda/Stack_cpp/stack.cpp
+++ students/Artyom_Tsanda/Stack_cpp/stack.cpp
@@ -13,6 +13,8 @@ struct stack_t
    ~stack_t();
     bool push(double value);
     double pop();
+    // FIXME It should be better to read with space between () and const;
+    // bool OK() const;
     bool OK()const;    //checks if evrth is ok whith stack
     bool dump()const;  //prints all elements of the stack
     int size()const;   //returns a number of elements in the stack
@@ -22,13 +24,18 @@ struct stack_t
 stack_t::stack_t():
     count_(0)
     {
+    // FIXME In C++ we use commands 'new' and 'delete' to deal with
+    // dynamic memory
         memset(data_,0,_STACKSIZE_);
+        // FIXME In C++ we input or output text with std::cout and std::cin.
         printf("Hello. Stack has been already launched.\n");
     }
 
 stack_t::~stack_t()
 {
+// FIXME It is useless to assign value to counter here.
     count_=-1;
+    // FIXME In C++ we input or output text with std::cout and std::cin.
     printf("Stack has been already destroyed. Good bye.\n");
 }
 
@@ -36,6 +43,10 @@ bool stack_t::push(double value)
 {
     if(count_>=_STACKSIZE_ || count_<0)
         return false;
+    // FIXME I prefer to split this. It is pretty difficult to understand
+    // all effects of this code.
+    // count_++;
+    // data_[count] = valeu;
     data_[count_++]=value;
     return true;
 }
@@ -43,6 +54,7 @@ bool stack_t::push(double value)
 double stack_t::pop()
 {
     if(count_<1 || count_>_STACKSIZE_){
+    // FIXME In C++ we input or output text with std::cout and std::cin.
         printf("Error: Impossible usage of pop() function.\n");
         assert(0);
     }
@@ -64,12 +76,14 @@ bool stack_t::dump() const
         if(count_>=1){
             int i=count_;
             printf("\nHere is your stack:\n");
+            // FIXME THis is very complicated loop. Rewrite it.
+            // In C++ you can 'for (int i = 0; i <....; i++) {...}'
             for(i--;i>=0;i--)
                 printf("%03d element : %f\n",count_-i,data_[i]);
             printf("\n");
             return true;
         }
-        else{}
+        else{}  // FIXME What is it?) {} THis code is very unclear.
             printf("\nStack is empty.\n");
             return true;
         }
@@ -106,3 +120,6 @@ double stack_t::get()const
     printf("%fl\n",st.get());
 
 }*/
+
+
+// FIXME Add list-based version!
